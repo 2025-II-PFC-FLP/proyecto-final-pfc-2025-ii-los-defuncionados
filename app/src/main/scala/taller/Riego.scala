3f005e7d57d735eed609c1c6c72c7b19c85e1e48
Sandreymv <Sandrey2028@gmail.com>
2025-12-03 23:09:28 -0500

    Implementamos comentarios, y la generacion random de fincas y distancias, las funciones que tienen random dado (personalizado) nos ayuda con las pruebas para reproducir el mismo escenario.

--- app/src/main/scala/taller/Riego.scala
+++ app/src/main/scala/taller/Riego.scala
@@ -11,5 +11,49 @@ object Riego {
   type Distancia = Vector[Vector[Int]] // Esta es la matriz de distancia entre los tablones
   type ProgRiego = Vector[Int] // Vector para la programacion de riego
   type TiempoInicioRiego = Vector[Int] // Vector con el tiempo de inicio del riego
+
+  // Es el random que utilizamos en algunas funciones
+  val random = new Random()
+
+
+  // Generamos una Finca totalmente aleatoria utilizando el random normal
+  def fincaAlAzar(long: Int): Finca = // Aqui implementamos el generar numero de tablones. (el parametro long define el numero de tablones)
+    Vector.fill(long)(
+      (random.nextInt(long * 2) + 1,
+        random.nextInt(long) + 1, // Aqui implementamos el generar duracion de riego (Este valor es el tiempo del regado)
+        random.nextInt(4) + 1)
+    )
+  // Generamos una Finca usando un Random dado (con el fin de las pruebas que se puedan repetir)
+  def fincaAlAzarconRandom(r: Random, long: Int): Finca = // Aqui tambien implementamos el generar numero de tablones.
+    Vector.fill(long)(
+      (r.nextInt(long * 2) + 1,
+        r.nextInt(long) + 1,
+      r.nextInt(4) + 1)
+    )
+
+  // Generamos una matriz de distancia simetrica con el Random normal
+  def distanciaAlAzar(long: Int): Distancia = { // Aqui implementamos el generar distancias
+    val v = Vector.fill(long, long)(random.nextInt(long * 3) + 1)
+    Vector.tabulate(long, long)((i, j) =>
+    if (i == j) 0
+    else if (i < j) v(i)(j)
+    else v(j)(i)
+    )
+  }
+  // Generamos una matriz de distancia con el Random pero ahora dado
+  def distanciaAlAzarconRandom(r: Random, long: Int): Distancia = {
+    val v = Vector.fill(long, long)(r.nextInt(long * 3) * 1)
+    Vector.tabulate(long, long)((i, j) =>
+    if (i == j) 0
+    else if (i < j) v(i)(j)
+    else v(j)(i)
+    )
+  }
+
+
+
+
+
+
 }
 
